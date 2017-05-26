e35e76eb9b7902c5c1c5dd8a2c151e783d837b63
589865 <stanleygorman@C02T44V8H03Q.fios-router.home>
2017-05-26 11:40:52 -0400

    cleaning up index

--- server.js
+++ server.js
@@ -3,13 +3,11 @@ var express = require('express'),
 
 var port = process.env.PORT || 8000;
 
-
+app.use(express.static('public'));
 
 app.get('*', function(req, res){
 	res.redirect('/');  //redirect back to index route
 });
-
-app.use(express.static('public'));
 
 app.listen(port, function() {
   console.log("Front end running ", port);
