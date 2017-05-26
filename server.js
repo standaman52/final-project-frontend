f1dea6c61fb47a7c361b84db903f538c254ea87c
589865 <stanleygorman@C02T44V8H03Q.fios-router.home>
2017-05-26 11:33:52 -0400

    adding port

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 var express = require('express'),
     app = express();
 
-var port = 8000;
+var port = process.env.PORT || 8000;
 
-app.use(express.static('public'));
+
 
 app.get('*', function(req, res){
 	res.redirect('/');  //redirect back to index route
 });
+
+app.use(express.static('public'));
 
 app.listen(port, function() {
   console.log("Front end running ", port);
